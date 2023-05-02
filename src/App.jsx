04374e52c958ba19f8f0ef4a226dc1b63ec1fd0c
sajibsaifullah <dusajibphy@gmail.com>
2023-05-02 14:34:49 +0600

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:5000/allData")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
-     <h2 className='text-primary text-center'>Hello Chef Recipe Hunter</h2>
-     <p>more details coming soon...</p>
+      <h2 className="text-primary text-center">Hello Chef Recipe Hunter</h2>
+      <p>more details coming soon...</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
