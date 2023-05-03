55223774213c3557f9f0a7af7eafb815d557f7c6
sajibsaifullah <dusajibphy@gmail.com>
2023-05-03 20:01:51 +0600

    added layout, header & footer

---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'>© 2023 Flaming Fork</p>
+        </div>
+    );
+};
+
+export default Footer;
