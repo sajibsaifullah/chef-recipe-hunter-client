cd96e7789a0d24775c87db8aad15ff79381713cf
sajibsaifullah <dusajibphy@gmail.com>
2023-05-03 19:48:46 +0600

    added routes & website title

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import router from "./routes/Routes.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
