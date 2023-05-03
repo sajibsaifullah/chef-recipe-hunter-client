754d7e18c8978fbf85c5a887100fdb242d3f9833
sajibsaifullah <dusajibphy@gmail.com>
2023-05-03 20:30:19 +0600

    added header data

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    );
+  return (
+    <Container>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mx-auto d-flex justify-content-center align-items-center gap-2">
+            <NavLink className="text-decoration-none" to='/'>Home</NavLink>
+            <NavLink className="text-decoration-none" to='/'>Blog</NavLink>
+          </Nav>
+          <Button variant="primary">Login</Button>
+        </Container>
+      </Navbar>
+    </Container>
+  );
 };
 
 export default Header;
