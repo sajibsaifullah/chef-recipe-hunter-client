4b17ceffe087d609e3c0b2b9fcd02b7bfd42f6ea
sajibsaifullah <dusajibphy@gmail.com>
2023-05-05 11:02:44 +0600

    loadedr fixed & footer updated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../pages/Footer/Footer';
 import Header from '../pages/Header/Header';
+import Spinner from 'react-bootstrap/Spinner';
 
 const MainLayout = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
+            <div className='text-center'>{navigation.state === 'loading' && <>{<Spinner animation="grow" />}</>}</div>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <div>
-            <p className='text-center mt-4'>© 2023 Flaming Fork</p>
-        </div>
-    );
+  return (
+    <div className="container bg-dark text-white text-center mt-4 py-3">
+      <p>© 2023 Flaming Fork. All rights reserved.</p>
+      <p>
+        DISCLAIMER: The recipes on this website are provided for informational
+        purposes only. Chef Recipe Hunter does not guarantee the accuracy or
+        completeness of any recipe or information provided on this site. We
+        strongly recommend that you follow food safety guidelines and use your
+        best judgement when cooking.
+      </p>
+      <p>
+        CONTACT US: If you have any questions, comments, or suggestions, please
+        don't hesitate to contact us at{" "}
+        <a href="mailto:info@chefrecipehunter.com">info@chefrecipehunter.com</a>
+        .
+      </p>
+      <p>
+        FOLLOW US: Stay up-to-date with the latest recipes and cooking tips by
+        following us on social media:
+      </p>
+      <FaFacebook></FaFacebook>
+      <FaTwitter className="mx-2"></FaTwitter>
+      <FaInstagram></FaInstagram>
+    </div>
+  );
 };
 
 export default Footer;
